19c8656d3a96905e02d4a9cd73feff0b69a5d0a1
Deedaworld <123740460+Deedaworld@users.noreply.github.com>
2024-02-16 11:03:43 +0900

    Update db.js

--- node-app/db/db.js
+++ node-app/db/db.js
@@ -7,7 +7,7 @@ const dbInfo = {
     host: 'init-mysql.czuy8ksq2pce.ap-northeast-2.rds.amazonaws.com',
     user: 'admin',
     password: 'test1234',
-    database: 'init_rds',
+    database: 'init__rds',
 };
 
 // connection 변수 선언 및 초기화
