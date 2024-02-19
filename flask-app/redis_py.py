36aa9eb2b56c87c3c8a0199e6494ccc4332ab79d
Deedaworld <123740460+Deedaworld@users.noreply.github.com>
2024-02-19 11:02:42 +0900

    Update redis_py.py

--- flask-app/redis_py.py
+++ flask-app/redis_py.py
@@ -8,7 +8,7 @@ from botocore.exceptions import ClientError
 app = Flask(__name__)
 CORS(app,supports_credentials=True)
 
-redis_host = '3.34.192.127'
+redis_host = 'init-redis-cluster.tfsu7r.ng.0001.apn2.cache.amazonaws.com'
 redis_port = 6379
 
 dynamodb = get_dynamodb_resource()
