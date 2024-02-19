6f5365ae5cab90b8de07dafcb03a9eeff900ae3f
Deedaworld <123740460+Deedaworld@users.noreply.github.com>
2024-02-19 15:55:43 +0900

    Update db.js

--- node-app/db/db.js
+++ node-app/db/db.js
@@ -4,10 +4,10 @@ const mysql = require('mysql');
 const util = require('util');
 //RDS 연동
 const dbInfo = {
-    host: 'init-mysql.czuy8ksq2pce.ap-northeast-2.rds.amazonaws.com',
+    host: 'init-serverless.cluster-czuy8ksq2pce.ap-northeast-2.rds.amazonaws.com',
     user: 'admin',
     password: 'test1234',
-    database: 'init__rds',
+    database: 'init_serverless',
 };
 
 // connection 변수 선언 및 초기화
